8c4f175aac90b4acc7fd21426a381cd0c28c39ec
Илья Мезенцев <i.mezentsev@wisetech.pro>
2019-05-18 22:28:15 +1200

    #2 bot: fixed mock

--- bot/talktome/test_controller.py
+++ bot/talktome/test_controller.py
@@ -14,10 +14,11 @@ class TestController(TestCase):
 		self.assertEqual(chat_id, ret_chat_id)
 		self.assertEqual(ret_str, "I do not understand you, mate!")
 
-	@patch('django.template.loader.render_to_string', return_value="mock_function")
-	def test_hello(self):
+	@patch('django.template.loader')
+	def test_hello(self, mock_loader):
 		chat_id = 87654321
 		ret_chat_id, ret_str = self.ctrl.processCommand(chat_id, "/info")
 
+	    # test that processCommand called render_to_string with the right parameters
+		mock_loader.render_to_string.assert_called_with('talktome/hello.md')
 		self.assertEqual(chat_id, ret_chat_id)
-		self.assertEqual(ret_str, "I do not understand you, mate")

a4cf2609cbbef8c88d1d98324d0ebf6012b817a9
laz <Yelizar.Huryn@gmail.com>
2019-08-10 21:57:16 +1200

    Work on us Email notifications update sessions initialization

--- webhook.py
+++ webhook.py
@@ -1,4 +1,4 @@
 import telepot
 bot_token = '867027665:AAGdRxN7xHNussXH5BvOTj_tvVXv_RGzvE4'
 bot = telepot.Bot(bot_token)
-bot.setWebhook('https://1a6dae7f.ngrok.io/tplatform/{bot_token}'.format(bot_token=bot_token))
+bot.setWebhook('https://82236e30.ngrok.io/tplatform/{bot_token}'.format(bot_token=bot_token))

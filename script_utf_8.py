0c2167444d4c5a41f13bf2d7f65e7306c3aff36d
Yelizar Huryn <Yelizar.Huryn@gmail.com>
2020-02-12 18:03:33 +0300

    new_branch

--- script_utf_8.py
+++ script_utf_8.py
@@ -1,5 +1,5 @@
 from django.db import connection
 cursor = connection.cursor()
 results=[]
-for row in cursor.fetchall(): results.append(row)
-for row in results[1:]: cursor.execute('ALTER TABLE %s CONVERT TO CHARACTER SET utf8 COLLATE     utf8_general_ci;' % (row[0]))
+	for row in cursor.fetchall(): results.append(row)
+	for row in results[1:]: cursor.execute('ALTER TABLE %s CONVERT TO CHARACTER SET utf8 COLLATE     utf8_general_ci;' % (row[0]))
